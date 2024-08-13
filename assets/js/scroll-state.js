b1a17d8f9b438f233468c17938b2555d419991c2
Mika Kuitunen <mika.kuitunen@hotmail.com>
2024-08-13 22:58:14 +0300

    Save scroll states during session

---
+++ assets/js/scroll-state.js
@@ -0,0 +1,49 @@
+const SCROLL_STATE_KEY = "scrollState";
+
+let sleep = false;
+
+const saveScrollState = () => {
+  const stateString = sessionStorage.getItem(SCROLL_STATE_KEY);
+  let state = {};
+  if (stateString) {
+    state = JSON.parse(stateString);
+  }
+  state[window.location.pathname] = window.scrollY;
+  sessionStorage.setItem(SCROLL_STATE_KEY, JSON.stringify(state));
+}
+
+const handleScroll = () => {
+  if (!sleep) {
+    saveScrollState();
+    sleep = true;
+    setTimeout(() => sleep = false, 300);
+  }
+};
+
+const restoreScrollState = () => {
+  const stateString = sessionStorage.getItem(SCROLL_STATE_KEY);
+  if (stateString) {
+    const state = JSON.parse(stateString);
+    const pageScrollState = state[window.location.pathname];
+    if (pageScrollState) {
+      window.scrollTo({
+        top: pageScrollState
+      })
+    }
+  }
+}
+
+const init = () => {
+  restoreScrollState();
+
+  document.addEventListener('scroll', handleScroll);
+  document.addEventListener('scrollend', saveScrollState);
+};
+
+if (document.readyState !== 'loading') {
+  init()
+} else {
+  document.addEventListener('DOMContentLoaded', () => {
+      init();
+  });
+}
